48c4c62675cad5910018ca69987e583d79f95cb5
Jessica Di Ponziano <jess@webdevjd.com>
2019-04-01 15:33:45 -0400

    Add create tokens method

--- js/player.js
+++ js/player.js
@@ -4,6 +4,23 @@ class Player {
     this.id = id;
     this.color = color;
     this.isTurn = isTurn;
-    this.tokens = [];
+    this.tokens = this.createTokens(21);
+  }
+
+  /**
+  * Creates token objects for player
+  * @param   {integer}   num - Number of token objects to be created
+  * @return  {array}     tokens - an arary of new token objects
+  */
+
+  createTokens(num) {
+    const tokens = [];
+
+    for (let i = 0; i < num; i++) {
+      let token = new Token(i, this);
+      tokens.push(token);
+    }
+
+    return tokens;
   }
 }
