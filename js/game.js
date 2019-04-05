ae495ce04684fefdde74017de2d548995cb22f4f
Jessica Di Ponziano <jess@webdevjd.com>
2019-04-05 16:25:17 -0400

    Add game constructor and create players method

--- js/game.js
+++ js/game.js
@@ -1,3 +1,17 @@
 class Game {
-  
+  constructor() {
+    this.board = new Board();
+    this.players = this.createPlayers();
+    this.ready = false;
+  }
+
+  /**
+  * Creates two player objects
+  * @return  {Array}    An array of two Player objects.
+  */
+  createPlayers() {
+    const players = [new Player('Josh', '#e15258', 1, true),
+                     new Player('Jess', '#e59a13', 2 )];
+    return players;
+  }
 }
