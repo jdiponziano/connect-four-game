8c667cae07171b9c12b770ad0f34b7be24e4256e
Jessica Di Ponziano <jess@webdevjd.com>
2019-04-11 12:08:46 -0400

    Add key event listener

--- js/app.js
+++ js/app.js
@@ -6,3 +6,12 @@ button.addEventListener('click', function() {
   this.style.display = 'none';
   document.getElementById('play-area').style.opacity = '1';
 });
+
+/** 
+ * Listen for keyboard presses
+ */
+
+document.addEventListener('keydown', function (event) {
+  console.log(event.key); //outputs a string naming the pressed key 
+  game.handleKeydown();
+});
