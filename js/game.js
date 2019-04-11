b1c8b41173a68d32cd993d4cb5259ed36e1bccb3
Jessica Di Ponziano <jess@webdevjd.com>
2019-04-11 12:09:10 -0400

    Create arrow left and right movements

--- js/game.js
+++ js/game.js
@@ -34,4 +34,20 @@ class Game {
     this.activePlayer.activeToken.drawHTMLToken();
     this.ready = true;
   }
+
+  /**
+  * Branches code, depending on what key player presses
+  * @param   {Object}    e - Keydown event object
+  */
+  handleKeydown(e) {
+    if(this.ready) {
+      if(e.key === 'ArrowLeft') {
+        this.activePlayer.activeToken.moveLeft();
+      } else if (e.key === 'ArrowRight') {
+        this.activePlayer.activeToken.moveRight(this.board.columns);
+      } else if (e.key === 'ArrowDown') {
+        //arrowdown
+      }
+    }
+  }
 }
