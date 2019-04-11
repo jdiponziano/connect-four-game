1f0e443d7b5326157cf54454abd3b0793b29bcb4
Jessica Di Ponziano <jess@webdevjd.com>
2019-04-10 22:06:26 -0400

    Add active and unused tokens method

--- js/player.js
+++ js/player.js
@@ -7,6 +7,21 @@ class Player {
     this.tokens = this.createTokens(21);
   }
 
+  /*
+  * Gets all tokens that haven't been dropped
+  * @return {array} Array of unused tokens
+  */
+  unusedTokens() {
+    return this.tokens.filter(token => !token.dropped);
+  }
+
+  /*
+  * Gets the first token from array of unused token objects.
+  * @return {Object} First token object in an array of unused tokens
+  */
+  activeToken() {
+    return unusedTokens[0];
+  }
   /**
   * Creates token objects for player
   * @param   {integer}   num - Number of token objects to be created
