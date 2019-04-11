789f24c3fa52ec108eb8cd528ce9b50efe062c4a
Jessica Di Ponziano <jess@webdevjd.com>
2019-04-10 22:06:42 -0400

    Create active player getter method and start game method

--- js/game.js
+++ js/game.js
@@ -6,6 +6,14 @@ class Game {
   }
 
   /**
+  * Returns active player
+  * @return  {Object} The active player object.
+  */
+  activePlayer() {
+    return players.find(player => player.isTurn);
+  }
+
+  /**
   * Creates two player objects
   * @return  {Array}    An array of two Player objects.
   */
@@ -14,4 +22,9 @@ class Game {
                      new Player('Jess', '#e59a13', 2 )];
     return players;
   }
+
+  /**
+  * Gets game ready to play
+  */
+  startGame() {}
 }
